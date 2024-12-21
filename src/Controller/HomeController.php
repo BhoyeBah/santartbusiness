dbb84f2f61ed7f50592e86b3384727e25ef2e864
Mamadou Bhoye Bah <bhoyemad11@gmail.com>
2024-12-21 11:28:09 +0000

    design

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,4 +13,29 @@ class HomeController extends AbstractController
     {
         return $this->render('home/accueil.html.twig');
     }
+
+    #[Route('/a-propos', name: 'app_about')]
+    public function about(): Response
+    {
+        return $this->render('home/about.html.twig');
+    }
+
+    #[Route('/produit', name: 'app_product')]
+    public function shop(): Response
+    {
+        return $this->render('home/product.html.twig');
+    }
+
+
+    #[Route('/blog', name: 'app_blog')]
+    public function blog(): Response
+    {
+        return $this->render('home/blog.html.twig');
+    }
+
+    #[Route('/contact', name: 'app_contact')]
+    public function contact(): Response
+    {
+        return $this->render('home/contact.html.twig');
+    }
 }
